11863a859b87fa2fc09502ed0ab84d71cd03e9ac
Jschwery <86555316+Jschwery@users.noreply.github.com>
2022-12-07 09:23:27 -0600

    Initial commit

---
+++ src/main/java/com/schwery/datawebapp/DatawebappApplication.java
@@ -0,0 +1,13 @@
+package com.schwery.datawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schwery/datawebapp/DatawebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schwery.datawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
